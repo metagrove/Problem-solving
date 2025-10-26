6b1385fbd0364b83827d57b4110c98925d8577c3
metagrove <echogrove.acc@gmail.com>
2025-10-26 21:18:02 +0530

    FILE UPDATED SUCESS

--- Problem_sol/Largest_Leader.py
+++ Problem_sol/Largest_Leader.py
@@ -1,12 +1,12 @@
-arr = [11,2,3,4,12,54,34,9]
+arr = [11,2,3,4,12,54,34,9]       #ARRAY OF ELEMENTS
 
-leader = []
+leader = []                       #LIST TO STORE LEADER ELEMENTS
 
-mx_ele = arr[-1]
-leader.append(mx_ele)
+mx_ele = arr[-1]                  #INITIALIZING MAXIMUM ELEMENT WITH LAST ELEMENT
+leader.append(mx_ele)             #ADDING LAST ELEMENT TO LEADER LIST
 
-for i in range(len(arr)-2,-1,-1):
-    if arr[i]>mx_ele:
-        mx_ele = arr[i]
-        leader.append(mx_ele)   
-print(leader)
+for i in range(len(arr)-2,-1,-1): #TRAVERSING THE ARRAY FROM SECOND LAST ELEMENT TO FIRST
+    if arr[i]>mx_ele:             #IF CURRENT ELEMENT IS GREATER THAN MAXIMUM ELEMENT
+        mx_ele = arr[i]           #UPDATING MAXIMUM ELEMENT
+        leader.append(mx_ele)     #ADDING CURRENT ELEMENT TO LEADER LIS
+print(leader)                     #PRINTING THE LEADER ELEMENTS
